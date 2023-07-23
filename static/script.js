79f1748793a3b61e875a4a786004f66760a8bc3d
Nishad Bhujbal <127653267+nishadbhujbal@users.noreply.github.com>
2023-07-24 00:17:16 +0530

    Update script.js
    Updated the js file for the animation to work.

--- static/script.js
+++ static/script.js
@@ -1,14 +1,1 @@
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    for (let i = 0; i < reveals.length; i++) {
-        let windowHeight = window.innerHeight;
-        let revealTop = reveals[i].getBoundingClientRect().top;
-        let revealPoint = 150;
-
-        if (revealTop < windowHeight - revealPoint) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
-    }
-}
+const swup = new Swup();
